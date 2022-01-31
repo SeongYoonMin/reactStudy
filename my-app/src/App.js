f018390f7201cf7bb082b770aef060e6dd28c6a9
Villain_uni <namo4862@naver.com>
2022-01-31 22:54:21 +0900

    [add] useEffect

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,31 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick  = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  const iRunOnlyOnce = () => {
+    alert(" Hello Runner.")
+  }
+  useEffect(iRunOnlyOnce, []);
+  useEffect(()=>{
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Search For ", keyword);
+    }
+  }, [keyword]);
+  useEffect(()=> {
+    console.log("add Counter");
+  }, [counter])
   return (
     <div>
+      <input value={keyword} type="text" placeholder="Search here..." onChange={onChange} />
       <h1 className={styles.title}>Welcome Back!</h1>
+      <h2>{counter}</h2>
       <Button text={"Continue"} backColor={"pink"} textColor={"white"}/>
+      <button onClick={onClick}>Click me</button>
     </div>
   );
 }
